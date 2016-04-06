6141851871138dd7424546d25ffee084ac296fe8
Alex Crawford <alex.crawford@coreos.com>
2016-04-06 14:50:34 -0700

    vendor: bump github.com/coreos/ignition/config

--- vendor/github.com/coreos/ignition/config/config.go
+++ vendor/github.com/coreos/ignition/config/config.go
@@ -16,68 +16,96 @@ package config
 
 import (
 	"bytes"
-	"compress/gzip"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
+
+	"github.com/coreos/ignition/config/types"
+	"github.com/coreos/ignition/config/v1"
 
 	"github.com/camlistore/camlistore/pkg/errorutil"
 )
 
-type Config struct {
-	Version  int      `json:"ignitionVersion"    yaml:"ignition_version"`
-	Storage  Storage  `json:"storage,omitempty"  yaml:"storage"`
-	Systemd  Systemd  `json:"systemd,omitempty"  yaml:"systemd"`
-	Networkd Networkd `json:"networkd,omitempty" yaml:"networkd"`
-	Passwd   Passwd   `json:"passwd,omitempty"   yaml:"passwd"`
+var (
+	ErrCloudConfig = errors.New("not a config (found coreos-cloudconfig)")
+	ErrEmpty       = errors.New("not a config (empty)")
+	ErrScript      = errors.New("not a config (found coreos-cloudinit script)")
+	ErrDeprecated  = errors.New("config format deprecated")
+)
+
+func Parse(rawConfig []byte) (types.Config, error) {
+	major, err := majorVersion(rawConfig)
+	if err != nil {
+		return types.Config{}, err
+	}
+
+	switch major {
+	case 1:
+		config, err := ParseFromV1(rawConfig)
+		if err != nil {
+			return types.Config{}, err
+		}
+
+		return config, ErrDeprecated
+	default:
+		return ParseFromLatest(rawConfig)
+	}
 }
 
-const (
-	Version = 1
-)
-
-var (
-	ErrVersion     = errors.New("incorrect config version")
-	ErrCloudConfig = errors.New("not a config (found coreos-cloudconfig)")
-	ErrScript      = errors.New("not a config (found coreos-cloudinit script)")
-	ErrEmpty       = errors.New("not a config (empty)")
-)
-
-func Parse(config []byte) (cfg Config, err error) {
-	if err = json.Unmarshal(config, &cfg); err == nil {
-		if cfg.Version != Version {
-			err = ErrVersion
-		}
-	} else if isCloudConfig(decompressIfGzipped(config)) {
-		err = ErrCloudConfig
-	} else if isScript(decompressIfGzipped(config)) {
-		err = ErrScript
-	} else if isEmpty(config) {
+func ParseFromLatest(rawConfig []byte) (config types.Config, err error) {
+	if err = json.Unmarshal(rawConfig, &config); err == nil {
+		err = config.Ignition.Version.AssertValid()
+	} else if isEmpty(rawConfig) {
 		err = ErrEmpty
+	} else if isCloudConfig(rawConfig) {
+		err = ErrCloudConfig
+	} else if isScript(rawConfig) {
+		err = ErrScript
 	}
 	if serr, ok := err.(*json.SyntaxError); ok {
-		line, col, highlight := errorutil.HighlightBytePosition(bytes.NewReader(config), serr.Offset)
+		line, col, highlight := errorutil.HighlightBytePosition(bytes.NewReader(rawConfig), serr.Offset)
 		err = fmt.Errorf("error at line %d, column %d\n%s%v", line, col, highlight, err)
 	}
 
 	return
 }
 
-func isEmpty(userdata []byte) bool {
-	return len(userdata) == 0
+func ParseFromV1(rawConfig []byte) (types.Config, error) {
+	config, err := v1.Parse(rawConfig)
+	if err != nil {
+		return types.Config{}, err
+	}
+
+	return TranslateFromV1(config)
 }
 
-func decompressIfGzipped(data []byte) []byte {
-	if reader, err := gzip.NewReader(bytes.NewReader(data)); err == nil {
-		uncompressedData, err := ioutil.ReadAll(reader)
-		reader.Close()
-		if err == nil {
-			return uncompressedData
-		} else {
-			return data
+type config struct {
+	Version  *int `json:"ignitionVersion" yaml:"ignition_version"`
+	Ignition struct {
+		Version *types.IgnitionVersion `json:"version,omitempty" yaml:"version" merge:"old"`
+	} `json:"ignition" yaml:"ignition"`
+}
+
+func majorVersion(rawConfig []byte) (int64, error) {
+	var composite config
+	if err := json.Unmarshal(rawConfig, &composite); err != nil {
+		if serr, ok := err.(*json.SyntaxError); ok {
+			line, col, highlight := errorutil.HighlightBytePosition(bytes.NewReader(rawConfig), serr.Offset)
+			err = fmt.Errorf("error at line %d, column %d\n%s%v", line, col, highlight, err)
 		}
-	} else {
-		return data
+		return 0, err
 	}
+
+	var major int64
+	if composite.Ignition.Version != nil {
+		major = composite.Ignition.Version.Major
+	} else if composite.Version != nil {
+		major = int64(*composite.Version)
+	}
+
+	return major, nil
+}
+
+func isEmpty(userdata []byte) bool {
+	return len(userdata) == 0
 }
