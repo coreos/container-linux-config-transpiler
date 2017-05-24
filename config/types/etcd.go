ac2ff6017861446be195b3adb444161e1cd5833e
Gyu-Ho Lee <gyuhox@gmail.com>
2017-05-24 09:22:37 -0700

    config/types: add etcd v3.2 (initial copy from 3.1)
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- config/types/etcd.go
+++ config/types/etcd.go
@@ -54,7 +54,7 @@ func (e EtcdVersion) Validate() report.Report {
 		return report.ReportFromError(EtcdVersionTooOld, report.EntryError)
 	case v.Major == 2 && v.Minor > 3:
 		fallthrough
-	case v.Major == 3 && v.Minor > 1:
+	case v.Major == 3 && v.Minor > 2:
 		return report.ReportFromError(EtcdMinorVersionTooNew, report.EntryWarning)
 	case v.Major > 3:
 		return report.ReportFromError(EtcdMajorVersionTooNew, report.EntryError)
@@ -101,8 +101,14 @@ func (etcd *Etcd) UnmarshalYAML(unmarshal func(interface{}) error) error {
 			return err
 		}
 		etcd.Options = o
-	} else if version.Major == 3 && version.Minor >= 1 {
+	} else if version.Major == 3 && version.Minor == 1 {
 		o := Etcd3_1{}
+		if err := unmarshal(&o); err != nil {
+			return err
+		}
+		etcd.Options = o
+	} else if version.Major == 3 && version.Minor >= 2 {
+		o := Etcd3_2{}
 		if err := unmarshal(&o); err != nil {
 			return err
 		}
@@ -195,6 +201,55 @@ type Etcd3_0 struct {
 }
 
 type Etcd3_1 struct {
+	Name                     string `yaml:"name"                        cli:"name"`
+	DataDir                  string `yaml:"data_dir"                    cli:"data-dir"`
+	WalDir                   string `yaml:"wal_dir"                     cli:"wal-dir"`
+	SnapshotCount            int    `yaml:"snapshot_count"              cli:"snapshot-count"`
+	HeartbeatInterval        int    `yaml:"heartbeat_interval"          cli:"heartbeat-interval"`
+	ElectionTimeout          int    `yaml:"election_timeout"            cli:"election-timeout"`
+	EnablePprof              bool   `yaml:"enable_pprof"                cli:"enable-pprof"`
+	ListenPeerUrls           string `yaml:"listen_peer_urls"            cli:"listen-peer-urls"`
+	ListenClientUrls         string `yaml:"listen_client_urls"          cli:"listen-client-urls"`
+	MaxSnapshots             int    `yaml:"max_snapshots"               cli:"max-snapshots"`
+	MaxWals                  int    `yaml:"max_wals"                    cli:"max-wals"`
+	Cors                     string `yaml:"cors"                        cli:"cors"`
+	InitialAdvertisePeerUrls string `yaml:"initial_advertise_peer_urls" cli:"initial-advertise-peer-urls"`
+	InitialCluster           string `yaml:"initial_cluster"             cli:"initial-cluster"`
+	InitialClusterState      string `yaml:"initial_cluster_state"       cli:"initial-cluster-state"`
+	InitialClusterToken      string `yaml:"initial_cluster_token"       cli:"initial-cluster-token"`
+	AdvertiseClientUrls      string `yaml:"advertise_client_urls"       cli:"advertise-client-urls"`
+	Discovery                string `yaml:"discovery"                   cli:"discovery"`
+	DiscoverySrv             string `yaml:"discovery_srv"               cli:"discovery-srv"`
+	DiscoveryFallback        string `yaml:"discovery_fallback"          cli:"discovery-fallback"`
+	DiscoveryProxy           string `yaml:"discovery_proxy"             cli:"discovery-proxy"`
+	StrictReconfigCheck      bool   `yaml:"strict_reconfig_check"       cli:"strict-reconfig-check"`
+	AutoCompactionRetention  int    `yaml:"auto_compaction_retention"   cli:"auto-compaction-retention"`
+	Proxy                    string `yaml:"proxy"                       cli:"proxy"`
+	ProxyFailureWait         int    `yaml:"proxy_failure_wait"          cli:"proxy-failure-wait"`
+	ProxyRefreshInterval     int    `yaml:"proxy_refresh_interval"      cli:"proxy-refresh-interval"`
+	ProxyDialTimeout         int    `yaml:"proxy_dial_timeout"          cli:"proxy-dial-timeout"`
+	ProxyWriteTimeout        int    `yaml:"proxy_write_timeout"         cli:"proxy-write-timeout"`
+	ProxyReadTimeout         int    `yaml:"proxy_read_timeout"          cli:"proxy-read-timeout"`
+	CaFile                   string `yaml:"ca_file"                     cli:"ca-file"                     deprecated:"ca_file obsoleted by trusted_ca_file and client_cert_auth"`
+	CertFile                 string `yaml:"cert_file"                   cli:"cert-file"`
+	KeyFile                  string `yaml:"key_file"                    cli:"key-file"`
+	ClientCertAuth           bool   `yaml:"client_cert_auth"            cli:"client-cert-auth"`
+	TrustedCaFile            string `yaml:"trusted_ca_file"             cli:"trusted-ca-file"`
+	AutoTls                  bool   `yaml:"auto_tls"                    cli:"auto-tls"`
+	PeerCaFile               string `yaml:"peer_ca_file"                cli:"peer-ca-file"                deprecated:"peer_ca_file obsoleted peer_trusted_ca_file and peer_client_cert_auth"`
+	PeerCertFile             string `yaml:"peer_cert_file"              cli:"peer-cert-file"`
+	PeerKeyFile              string `yaml:"peer_key_file"               cli:"peer-key-file"`
+	PeerClientCertAuth       bool   `yaml:"peer_client_cert_auth"       cli:"peer-client-cert-auth"`
+	PeerTrustedCaFile        string `yaml:"peer_trusted_ca_file"        cli:"peer-trusted-ca-file"`
+	PeerAutoTls              bool   `yaml:"peer_auto_tls"               cli:"peer-auto-tls"`
+	Debug                    bool   `yaml:"debug"                       cli:"debug"`
+	LogPackageLevels         string `yaml:"log_package_levels"          cli:"log-package-levels"`
+	ForceNewCluster          bool   `yaml:"force_new_cluster"           cli:"force-new-cluster"`
+	Metrics                  string `yaml:"metrics"                     cli:"metrics"`
+	LogOutput                string `yaml:"log_output"                  cli:"log-output"`
+}
+
+type Etcd3_2 struct {
 	Name                     string `yaml:"name"                        cli:"name"`
 	DataDir                  string `yaml:"data_dir"                    cli:"data-dir"`
 	WalDir                   string `yaml:"wal_dir"                     cli:"wal-dir"`
